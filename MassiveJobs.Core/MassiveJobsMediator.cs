758a04ec669a42c4d7961b426a342ec56ac3e024
enadzan <2304785+enadzan@users.noreply.github.com>
2020-07-08 22:45:09 +0200

    Refactor MassiveJobsMediator - publisher and workers are protected

--- MassiveJobs.Core/MassiveJobsMediator.cs
+++ MassiveJobs.Core/MassiveJobsMediator.cs
@@ -16,46 +16,45 @@ namespace MassiveJobs.Core
             }
         }
         
-        private readonly ILogger<MassiveJobsMediator> _logger;
 
         private readonly IServiceScopeFactory _scopeFactory;
         private readonly IServiceScope _scope;
 
-        private readonly IJobPublisher _publisher;
-        private readonly IWorkerCoordinator _workers;
+        protected readonly ILogger<MassiveJobsMediator> Logger;
+        protected readonly IJobPublisher Publisher;
+        protected readonly IWorkerCoordinator Workers;
 
         public MassiveJobsMediator(IServiceScopeFactory scopeFactory, ILogger<MassiveJobsMediator> logger)
         {
             _scopeFactory = scopeFactory;
-            _logger = logger;
-
             _scope = scopeFactory.CreateScope();
 
-            _publisher = _scope.GetService<IJobPublisher>();
-            _workers = _scope.GetService<IWorkerCoordinator>();
+            Logger = logger;
+            Publisher = _scope.GetService<IJobPublisher>();
+            Workers = _scope.GetService<IWorkerCoordinator>();
         }
 
         public void Dispose()
         {
-            _workers.StopJobWorkers();
+            Workers.StopJobWorkers();
 
-            _scope.SafeDispose(_logger);
-            _scopeFactory.SafeDispose(_logger);
+            _scope.SafeDispose(Logger);
+            _scopeFactory.SafeDispose(Logger);
         }
 
         public void Publish(IEnumerable<JobInfo> jobs)
         {
-            _publisher.Publish(jobs);
+            Publisher.Publish(jobs);
         }
 
         public void StartJobWorkers()
         {
-            _workers.StartJobWorkers();
+            Workers.StartJobWorkers();
         }
 
         public void StopJobWorkers()
         {
-            _workers.StopJobWorkers();
+            Workers.StopJobWorkers();
         }
     }
 }
