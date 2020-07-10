4258dd208567ee278cab1f383e7fcb2155c6f851
enadzan <2304785+enadzan@users.noreply.github.com>
2020-07-10 19:44:21 +0200

    Add MassiveJobs.Core.Hosting project
    This should simplify running MassiveJobs in hosted environment in .net core

---
+++ MassiveJobs.Core.Hosting/LoggerWrapper.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace MassiveJobs.Core.Hosting
+{
+    class LoggerFactoryWrapper : IJobLoggerFactory
+    {
+        private readonly ILoggerFactory _msLoggerFactory;
+
+        public LoggerFactoryWrapper(ILoggerFactory msLoggerFactory)
+        {
+            _msLoggerFactory = msLoggerFactory;
+        }
+
+        public IJobLogger<TCategory> CreateLogger<TCategory>()
+        {
+            return new LoggerWrapper<TCategory>(_msLoggerFactory.CreateLogger<TCategory>());
+        }
+    }
+
+    class LoggerWrapper<TCategory> : IJobLogger<TCategory>
+    {
+        private readonly ILogger _msLogger;
+
+        public LoggerWrapper(ILogger<TCategory> msLogger)
+        {
+            _msLogger = msLogger;
+        }
+
+        public bool IsEnabled(JobLogLevel logLevel)
+        {
+            return _msLogger.IsEnabled(ConvertLogLevel(logLevel));
+        }
+
+        public void Log(JobLogLevel logLevel, Exception exception, string message)
+        {
+            _msLogger.Log(ConvertLogLevel(logLevel), exception, message);
+        }
+
+        private LogLevel ConvertLogLevel(JobLogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case JobLogLevel.Trace:
+                    return LogLevel.Trace;
+                case JobLogLevel.Debug:
+                    return LogLevel.Debug;
+                case JobLogLevel.Information:
+                    return LogLevel.Information;
+                case JobLogLevel.Warning:
+                    return LogLevel.Warning;
+                case JobLogLevel.Error:
+                    return LogLevel.Error;
+                case JobLogLevel.Critical:
+                    return LogLevel.Critical;
+                default:
+                    return LogLevel.None;
+            }
+        }
+    }
+}
---
+++ MassiveJobs.Core.Hosting/ServiceScopeWrapper.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MassiveJobs.Core.Hosting
+{
+    public class ServiceScopeWrapper : IJobServiceScope
+    {
+        private readonly IServiceScope _scope;
+
+        public ServiceScopeWrapper(IServiceScope scope)
+        {
+            _scope = scope;
+        }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
+        }
+
+        public object GetRequiredService(Type type)
+        {
+            return _scope.ServiceProvider.GetRequiredService(type);
+        }
+
+        public object GetService(Type type)
+        {
+            return _scope.ServiceProvider.GetService(type);
+        }
+    }
+
+    public class ServiceScopeFactoryWrapper : IJobServiceScopeFactory
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public ServiceScopeFactoryWrapper(IServiceScopeFactory factory)
+        {
+            _scopeFactory = factory;
+        }
+
+        public IJobServiceScope CreateScope()
+        {
+            return new ServiceScopeWrapper(_scopeFactory.CreateScope());
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
+
