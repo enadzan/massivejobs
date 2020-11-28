e43dc0259d80122b6c64e36521d209eff57be7e8
enadzan <2304785+enadzan@users.noreply.github.com>
2020-11-28 16:53:49 +0100

    Move NamePrefix to MassiveJobsSettings

--- MassiveJobs.Core/MassiveJobsSettings.cs
+++ MassiveJobs.Core/MassiveJobsSettings.cs
@@ -2,6 +2,18 @@
 {
     public class MassiveJobsSettings
     {
+        public MassiveJobsSettings(string namePrefix = "")
+        {
+            NamePrefix = namePrefix;
+            ImmediateQueueNameTemplate = namePrefix + "massivejobs.immediate.{0:#00}";
+            LongRunningQueueNameTemplate = namePrefix = "massivejobs.long-running.{0:#00}";
+            ScheduledQueueNameTemplate = namePrefix + "massivejobs.scheduled.{0:#00}";
+            PeriodicQueueNameTemplate = namePrefix + "massivejobs.periodic.{0:#00}";
+            ErrorQueueName = namePrefix + "massivejobs.error";
+            FailedQueueName = namePrefix + "massivejobs.failed";
+            StatsQueueName = namePrefix + "massivejobs.stats";
+        }
+
         public int MaxDegreeOfParallelismPerWorker { get; set; } = 2;
 
         public int ImmediateWorkersCount { get; set; } = 2;
@@ -16,16 +28,21 @@
         public ushort ScheduledWorkersBatchSize { get; set; } = 100;
         public ushort PeriodicWorkersBatchSize { get; set; } = 100;
 
-        public string ImmediateQueueNameTemplate { get; set; } = "massivejobs.immediate.{0:#00}";
-        public string LongRunningQueueNameTemplate { get; set; } = "massivejobs.long-running.{0:#00}";
-        public string ScheduledQueueNameTemplate { get; set; } = "massivejobs.scheduled.{0:#00}";
-        public string PeriodicQueueNameTemplate { get; set; } = "massivejobs.periodic.{0:#00}";
+        public string ImmediateQueueNameTemplate { get; }
+        public string LongRunningQueueNameTemplate { get; }
+        public string ScheduledQueueNameTemplate { get; }
+        public string PeriodicQueueNameTemplate { get; }
 
-        public string ErrorQueueName { get; set; } = "massivejobs.error";
-        public string FailedQueueName { get; set; } = "massivejobs.failed";
-        public string StatsQueueName { get; set; } = "massivejobs.stats";
+        public string ErrorQueueName { get; }
+        public string FailedQueueName { get; }
+        public string StatsQueueName { get; }
 
         public int MaxQueueLength { get; set; } = QueueLength.Default;
+
+        /// <summary>
+        /// Prefix to be appended to all the queue names.
+        /// </summary>
+        public string NamePrefix { get; }
     }
 
     public static class QueueLength
